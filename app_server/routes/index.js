1196b9897a6bae312c112d7681d127b9dc8e5974
XiaoxuanW <wangxiaoxuan4869@gmail.com>
2018-12-09 19:51:16 -0800

    Adding static view templates

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main')
+var ctrlLocation = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
 /* GET home page. */
-router.get('/', ctrlMain.index);
+router.get('/', ctrlLocation.homelist);
+router.get('/locations', ctrlLocation.locationInfo);
+router.get('/locations/review/new', ctrlLocation.addReview);
+
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
